02fe1a46ad8e333d0ca69da53811307e7e1078b7
Maksim Bukhalo <maksimbukhalo1314@gmail.com>
2020-10-12 09:12:40 +0300

    feat():Add PathFinding

---
+++ Assets/PathFinding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFinding : MonoBehaviour
+{
+    public Transform[] points;
+
+    private NavMeshAgent _nav;
+    private int _destPoint;
+
+	private void Start()
+	{
+		_nav = gameObject.GetComponent<NavMeshAgent>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (!_nav.pathPending && _nav.remainingDistance < 0.5f)
+		{
+			GoToNextPoint();
+		}
+	}
+
+	private void GoToNextPoint()
+	{
+		if (points.Length == 0)
+		{
+			return;
+		}
+		_nav.destination = points[_destPoint].position;
+		_destPoint = (_destPoint + 1) % points.Length;
+	}
+}
