82360729bd69a6dcfee953d22ad63370f4113bc3
Maksim Bukhalo <maksimbukhalo1314@gmail.com>
2020-10-13 17:27:58 +0300

    feat():Added slow movement in water areas

---
+++ Assets/Sqripts/TriggerPlayerMask.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TriggerPlayerMask : MonoBehaviour
+{
+	private NavMeshAgent agent;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.layer == 4)
+		{
+			agent.speed -= 20;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.layer == 4)
+		{
+			agent.speed += 20;
+		}
+	}
+}
